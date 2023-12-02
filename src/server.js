b89c38caaee5866fd3ca0c624c3220b021b707f5
Dev <recoltare.unu@gmail.com>
2023-12-02 16:39:49 +0200

    s

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ async function authAsync() {
   return await supaClient.auth.signInWithOAuth({
     provider: "google",
     options: {
+      scopes: "https://www.googleapis.com/auth/drive",
       redirectTo: await getAuthUrl(),
     },
   });
