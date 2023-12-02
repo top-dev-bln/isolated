e96d047f1edf5cda0b867a13fcb4d1c408716c0f
Dev <recoltare.unu@gmail.com>
2023-12-02 19:32:34 +0200

    fdg

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
-import dotenv from "dotenv";
-dotenv.config();
-const supaAnonKey = import.meta.env.SUPABASE_ANON_KEY;
-const supaUrl = import.meta.env.SUPABASE_URL;
+const supaUrl = import.meta.env.VITE_SUPABASE_URL;
+const supaAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-//import { createClient } from "@supabase/supabase-js";
-//const supaClient = createClient(supaUrl, supaAnonKey);
-console.log(supaUrl);
-/*
+import { createClient } from "@supabase/supabase-js";
+const supaClient = createClient(supaUrl, supaAnonKey);
+
 async function authAsync() {
   return await supaClient.auth.signInWithOAuth({
     provider: "google",
-    options: { scope: "https://www.googleapis.com/auth/drive" },
+    options: { scopes: ["https://www.googleapis.com/auth/drive"] },
   });
-}*/
-
-async function authAsync() {
-  console.log("supa");
-  console.log(import.meta.env.BAKKA);
 }
 
 export { authAsync };
