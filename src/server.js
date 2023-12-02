e7ca4a93b20b150c0dde6d44e0f134ffffc7c686
Dev <recoltare.unu@gmail.com>
2023-12-02 19:50:37 +0200

    sss

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-    const supabaseAnonKey =
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-        async function authAsync() {
-            const supaClient = createClient(supabaseUrl, supabaseAnonKey);
-            return await supaClient.auth.signInWithOAuth({
-                provider: "google",
-            });
-        }
+async function authAsync() {
+  const supaClient = createClient(supabaseUrl, supabaseAnonKey);
+  return await supaClient.auth.signInWithOAuth({
+    provider: "google",
+  });
+}
 
 export { authAsync };
