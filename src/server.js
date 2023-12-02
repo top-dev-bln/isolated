493953deec208107582b64efc40b436a1a88f9f9
Dev <recoltare.unu@gmail.com>
2023-12-02 19:34:44 +0200

    adfsfa

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
-const supaUrl = import.meta.env.VITE_SUPABASE_URL;
-const supaAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
 import { createClient } from "@supabase/supabase-js";
-const supaClient = createClient(supaUrl, supaAnonKey);
+const supaClient = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
 async function authAsync() {
   return await supaClient.auth.signInWithOAuth({
     provider: "google",
-    options: { scopes: ["https://www.googleapis.com/auth/drive"] },
   });
 }
 
