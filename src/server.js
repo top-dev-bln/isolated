e40b9d77ee913f41b3201d87171ce5761e37d8d5
Dev <recoltare.unu@gmail.com>
2023-12-02 19:14:59 +0200

    gfd

--- src/server.js
+++ src/server.js
@@ -1,29 +1,22 @@
-//import .env
-//import dotenv from "dotenv";
-//dotenv.config();
-//const supaAnonKey = process.env.SUPABASE_ANON_KEY;
-//const supaUrl = process.env.SUPABASE_URL;
+import dotenv from "dotenv";
+dotenv.config();
+const supaAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supaUrl = import.meta.env.SUPABASE_URL;
 
-const supaUrl = "https://rovmgxgudxfaxrcuahos.supabase.co";
-const supaAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvdm1neGd1ZHhmYXhyY3VhaG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEyNzMyNzEsImV4cCI6MjAxNjg0OTI3MX0.B3ATVowsmCJYXc72AEP7ys553PjB-aO0ZoYnAe4ljpM";
-
-import { createClient } from "@supabase/supabase-js";
-const supaClient = createClient(supaUrl, supaAnonKey);
-
-async function getAuthUrl() {
-  return fetch("https://server-upldfy.vercel.app/griveAuth")
-    .then((res) => res.text())
-    .then((text) => {
-      return text;
-    });
-}
-
+//import { createClient } from "@supabase/supabase-js";
+//const supaClient = createClient(supaUrl, supaAnonKey);
+console.log(supaUrl);
+/*
 async function authAsync() {
   return await supaClient.auth.signInWithOAuth({
     provider: "google",
-    options: { redirectTo: await getAuthUrl() },
+    options: { scope: "https://www.googleapis.com/auth/drive" },
   });
+}*/
+
+async function authAsync() {
+  console.log("supa");
+  console.log(import.meta.env.BAKKA);
 }
 
 export { authAsync };
