da2f62d307e88274a4f7ac07dfd37abaf695651d
balanicornul <112486965+balanicornul@users.noreply.github.com>
2023-12-04 11:38:14 +0000

    fsa

--- src/server.js
+++ src/server.js
@@ -5,11 +5,17 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 
 // make the consent  screen with scopes to get access to google drive
+// do not use my server for this use scopes in supabase
+
+
 function LoginWithGoogle(){
   const supaClient = createClient(supabaseUrl, supabaseAnonKey);
   supaClient.auth.signInWithOAuth({
     provider: 'google',
     options: {
+      scopes: [
+        'https://www.googleapis.com/auth/drive',
+      ],
       queryParams: {
         access_type: 'offline',
         prompt: 'consent',
