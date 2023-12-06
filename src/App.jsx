d526b19a260304e3f6017ab67fcc403104255875
Dev <recoltare.unu@gmail.com>
2023-12-06 19:15:25 +0200

    Add console.log for code parameter

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   async function checkUserOnStart() {
     const url = new URL(window.location);
     const code = url.searchParams.get("code");
+    if (code) console.log(code);
     await supaClient.auth.onAuthStateChange((event, session) => {
       setIsAuthenticated(!!session);
       if (session) {
