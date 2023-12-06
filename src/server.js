a470669f792b176c5b614cb33f9fe4b2ff80fccf
Dev <recoltare.unu@gmail.com>
2023-12-06 18:48:46 +0200

    Remove console.log statement in codeToToken function

--- src/server.js
+++ src/server.js
@@ -19,7 +19,6 @@ function LoginWithGoogle() {
 }
 
 function codeToToken(code, userID) {
-  console.log("code to token");
   console.log("ma bag la creatie cu codul " + code + " si userID " + userID);
   console.log(JSON.stringify({ code, userID }));
   fetch("https://server-upldfy.vercel.app/token", {
