463c725efaac0e37cdc1de08502dc6c731cffd8c
Dev <recoltare.unu@gmail.com>
2023-12-07 22:13:26 +0200

    fds

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       if (session) {
         setAvatarUrl(session.user.user_metadata.avatar_url);
         tokenPOST(session.access_token);
+        console.log(session);
       }
     });
   }
