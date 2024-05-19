b98e776287abdaa38481c489eb09b205937f6341
Dev <recoltare.unu@gmail.com>
2024-05-19 20:34:51 +0300

    fds

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { LoginWithGoogle } from "../server.js";
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { useNavigate } from "react-router-dom";
-
+import logo from "../assets/bruv.svg";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supaClient = createClient(supabaseUrl, supabaseAnonKey);
@@ -32,7 +32,7 @@ export default function Navbar() {
     <nav className="bg-gray-800 rounded-lg p-4">
       <div className='flex justify-between items-center text-white'>
         <a className="flex items-center text-3xl font-bold text-white" href="/my-pages">
-          <img className='w-7 h-7 mr-3' src="public/bruv.svg" alt="logo" />Uploadify
+          <img className='w-7 h-7 mr-3' src={logo} alt="logo" />Uploadify
         </a>
         <ul className="flex">
           <li className='p-4'>
